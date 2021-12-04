ff3f05bbb367dded7dc13e71aa70651f3c029b07
Thomas Klausner <domm@plix.at>
2021-12-04 13:45:24 +0100

    cleanup, unifiy 04_2 to report both solutions

--- 2021/04_1.pl
+++ 2021/04_1.pl
@@ -1,45 +1,43 @@
 use 5.030;
 use strict;
 use warnings;
-use utf8;
 
-my @numbers = split (/,/,<>);
+my @numbers = split( /,/, <> );
 my @boards;
-my %drawn;
 
-my ($b,$r)=0;
+my ( $b, $r ) = 0;
 for (<>) {
     chomp;
-    if ($_ !~/\d/) {
+    if ( $_ !~ /\d/ ) { # newline, so start a new board
         $b++;
-        $r=0;
+        $r = 0;
         next;
     }
     s/^ //;
-    my @num = split(/ +/,$_);
-    for (my $c=0;$c<5;$c++) {
+    my @num = split( / +/, $_ );
+    for ( my $c = 0; $c < 5; $c++ ) {
         my $val = $num[$c];
         $boards[$b]->[$r]->{$val} = $val;
-        $boards[$b]->[$c+5]->{$val} = $val;
+        $boards[$b]->[ $c + 5 ]->{$val} = $val;
     }
     $r++;
 }
-my $nope = shift @boards;
+shift @boards;
 
 for my $drawn (@numbers) {
     for my $board (@boards) {
-        for my $line ($board->@*) {
-            if ($line->{$drawn}) {
+        for my $line ( $board->@* ) {
+            if ( exists $line->{$drawn} ) {
                 $line->{$drawn} = 'X';
             }
 
-            my @checked = grep { /X/ } values $line->%*;
-            if (@checked == 5) {
+            my @checked = grep {/X/} values $line->%*;
+            if ( @checked == 5 ) {
                 # WINNER!
                 my $sum;
-                for my $r (0..4) {
-                    for my $n (keys $board->[$r]->%*) {
-                        $sum+=$n unless $board->[$r]{$n} eq 'X';
+                for my $r ( 0 .. 4 ) {
+                    for my $n ( keys $board->[$r]->%* ) {
+                        $sum += $n unless $board->[$r]{$n} eq 'X';
                     }
                 }
                 say $sum;
--- 2021/04_2.pl
+++ 2021/04_2.pl
@@ -1,58 +1,55 @@
 use 5.030;
 use strict;
 use warnings;
-use utf8;
 
-my @numbers = split (/,/,<>);
+my @numbers = split( /,/, <> );
 my @boards;
-my %drawn;
 
-my ($b,$r)=0;
+my ( $b, $r ) = 0;
 for (<>) {
     chomp;
-    if ($_ !~/\d/) {
+    if ( $_ !~ /\d/ ) { # newline, so start a new board
         $b++;
-        $r=0;
+        $r = 0;
         next;
     }
     s/^ //;
-    my @num = split(/ +/,$_);
-    for (my $c=0;$c<5;$c++) {
+    my @num = split(/ +/);
+    for ( my $c = 0; $c < @num; $c++ ) {
         my $val = $num[$c];
         $boards[$b]->[$r]->{$val} = $val;
-        $boards[$b]->[$c+5]->{$val} = $val;
+        $boards[$b]->[ $c + @num ]->{$val} = $val;
     }
     $r++;
 }
-my $nope = shift @boards;
+shift(@boards);
 
-my $winners=0;
+my @results;
+
 for my $drawn (@numbers) {
-    for (my $i=0;$i<@boards;$i++) {
+    for ( my $i = 0; $i < @boards; $i++ ) {
         my $board = $boards[$i];
         next if $board eq 'winner';
-        for my $line ($board->@*) {
-            if (exists $line->{$drawn}) {
+        for my $line ( $board->@* ) {
+            if ( exists $line->{$drawn} ) {
                 $line->{$drawn} = 'X';
             }
 
-            my @checked = grep { /X/ } values $line->%*;
-            if (@checked == 5) {
-                $boards[$i]='winner';
-                $winners++;
-say "winner $i";
-                #         if ($winners == @boards) {
-                    my $sum;
-                    for my $r (0..4) {
-                        for my $n (keys $board->[$r]->%*) {
-                            $sum+=$n unless $board->[$r]{$n} eq 'X';
-                        }
+            my @checked = grep {/X/} values $line->%*;
+            if ( @checked == 5 ) {
+                $boards[$i] = 'winner';
+                my $sum;
+                for my $r ( 0 .. 4 ) {
+                    for my $n ( keys $board->[$r]->%* ) {
+                        $sum += $n unless $board->[$r]{$n} eq 'X';
                     }
-                    say "drawn $drawn, sum $sum, val ". $sum * $drawn;
-                    #   exit;
-                    #   }
+                }
+                push(@results, $sum * $drawn);
             }
         }
     }
 }
+
+say "part 1: ".$results[0];
+say "part 2: ".$results[-1];
 
