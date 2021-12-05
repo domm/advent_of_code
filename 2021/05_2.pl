52467439b5cd3dd193229ffab045cd5aadb483cc
Thomas Klausner <domm@plix.at>
2021-12-05 16:39:28 +0100

    cleaned up

--- 2021/05_2.pl
+++ 2021/05_2.pl
@@ -5,24 +5,22 @@ use warnings;
 my @map;
 for (<>) {
     my ($c1,$r1,$c2,$r2) = $_ =~ /(\d+),(\d+) -> (\d+),(\d+)/;
-    my ($cs1,$cs2) = sort { $a <=> $b } ($c1,$c2);
-    my ($rs1,$rs2)  = sort { $a <=> $b } ($r1,$r2);
+    my $dir_r = $r2 <=> $r1;
+    my $dir_c = $c2 <=> $c1;
 
     if ($r1 == $r2) {
-        for my $lr (0 .. abs($c1 - $c2)) {
-            $map[$r1][$cs1 + $lr]++;
+        for my $d (0 .. abs($c1 - $c2)) {
+            $map[$r1][$c1 + ($d * $dir_c)]++;
         }
     }
     elsif ($c1 == $c2) {
-        for my $ud (0 .. abs ($r1 - $r2)) {
-            $map[ $rs1 + $ud ][$c1]++;
+        for my $d (0 .. abs ($r1 - $r2)) {
+            $map[ $r1 + ($d * $dir_r) ][$c1]++;
         }
     }
     else {
-        my $dirr = $r2 <=> $r1;
-        my $dirc = $c2 <=> $c1;
         for my $d (0 .. abs ($r1 - $r2)) {
-            $map[ $r1 + ( $d * $dirr) ][ $c1 + ( $d * $dirc) ]++;
+            $map[ $r1 + ( $d * $dir_r) ][ $c1 + ( $d * $dir_c) ]++;
         }
     }
 }
