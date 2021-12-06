421f372d489210727bcaca5dab39962c3efde0eb
Thomas Klausner <domm@plix.at>
2021-12-06 16:02:01 +0100

    more cleanup

--- 2021/06_2.pl
+++ 2021/06_2.pl
@@ -2,13 +2,13 @@ use 5.030;
 use strict;
 use warnings;
 
-my %fish;
+my %fish = map { $_ => 0 } ( 0 .. 8 );
 for ( split( ',', <> ) ) {
     $fish{ int $_ }++;
 }
 
 for my $day ( 1 .. 256 ) {
-    my %next;
+    my %next  = ( 6 => 0, 8 => 0 );
     my $total = 0;
     for my $fish ( sort keys %fish ) {
         my $count = $fish{$fish};
@@ -24,6 +24,6 @@ for my $day ( 1 .. 256 ) {
     }
     $fish{6} += $next{6};
     $fish{8} = $next{8};
-    say "day $day: $total";
+    say "day $day: $total" if ( $day == 80 || $day == 256 );
 }
 
