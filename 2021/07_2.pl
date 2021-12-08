7c32b8a7b1c92d48cecdb6ed17064a6703e6347f
Thomas Klausner <domm@plix.at>
2021-12-08 21:44:32 +0100

    shorter (but still readable) day 7 part 2

--- 2021/07_2.pl
+++ 2021/07_2.pl
@@ -6,18 +6,16 @@ use Statistics::Basic qw(:all);
 
 my @crabs = split( ',', <> );
 my $mean  = mean(@crabs);
-my $low   = int($mean);
-my $high  = sprintf( '%.0f', $mean );
 
-my $lowest = 99999999999999999;
-for my $cand ( $low, $high ) {
+my $res=0;
+for my $cand ( int($mean), sprintf( '%.0f', $mean ) ) {
     my $fuel;
     for my $c (@crabs) {
         my $diff = abs( $c - $cand );
         $fuel += $diff * ( $diff + 1 ) / 2;
     }
-    $lowest = $fuel if $fuel < $lowest;
+    $res = !$res ? $fuel : $fuel < $res ? $fuel : $res;
 }
 
-say $lowest;
+say $res;
 
