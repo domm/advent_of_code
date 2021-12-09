776d681e94fa8940f9b26e40a9a3d39edca5a511
Thomas Klausner <domm@plix.at>
2021-12-09 14:14:50 +0100

    more array fixes

--- 2021/09_2.pl
+++ 2021/09_2.pl
@@ -20,21 +20,52 @@ for (my $r=0;$r<@map;$r++) {
         my $heat = $map[$r]->[$c];
         my $lower=0;
         for my $move ([-1,0],[1,0],[0,-1],[0,1]) {
-            my $val = $map[$r + $move->[0]]->[$c + $move->[1]];
-            if (not defined $val) {
+            my $r2 = $r + $move->[0];
+            my $c2 = $c + $move->[1];
+            my $val = $map[$r2]->[$c2];
+            if ($r2<0 || $c2<0) {
+                $lower++;
+            }
+            elsif (not defined $val) {
                 $lower++;
             }
             elsif ($heat < $val) {
                 $lower++;
             }
-            if ($lower == 4) {
-                $hits{$r.':'.$c} = $heat;
-                $sum += $heat +1;
-            }
+        }
+        if ($lower == 4) {
+            $hits{$r.':'.$c} = $heat;
+            $sum += $heat +1;
         }
     }
 }
 
 say $sum;
+
+__END__
+foreach my $sink (keys %hits) {
+    my $size;
+    my ($r,$c)=split(/:/,$sink);
+
+    walk($r, $c);
+
+}
+
+sub walk {
+    my ($r, $c,  ) = @_;
+    for my $move ([-1,0],[1,0],[0,-1],[0,1]) {
+        my $val = $map[$r + $move->[0]]->[$c + $move->[1]];
+        say ($r + $move->[0])." ".($c + $move->[1]).": $val";
+
+        
+
+    }
+    
+
+
+}
+
+
+use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \%hits;
 
 
