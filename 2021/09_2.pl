d3641b461aebd313e0b0d7aceb8b3e8dc74b8dc6
Thomas Klausner <domm@plix.at>
2021-12-09 14:05:25 +0100

    use array instead of hash for map

--- 2021/09_2.pl
+++ 2021/09_2.pl
@@ -2,34 +2,35 @@ use 5.030;
 use strict;
 use warnings;
 
-my %map;
+my @map;
 my ($r)=0;
 for (<>) {
     chomp;
     my $c=0;
     for (split(//)) {
-        $map{$r.':'.$c++} = $_;
+        $map[$r]->[$c++] = $_;
     }
     $r++;
 }
 
 my ($risk,$sum)=(0,0);
 my %hits;
-while (my ($loc,$heat) = each %map) {
-    my ($r,$c)=split(/:/,$loc);
-    my $lower=0;
-    for my $move ([-1,0],[1,0],[0,-1],[0,1]) {
-        my $look = ($r + $move->[0] ).':'.($c + $move->[1]);
-        my $val = $map{ $look};
-        if (not defined $val) {
-            $lower++;
-        }
-        elsif ($heat < $val) {
-            $lower++;
-        }
-        if ($lower == 4) {
-            $hits{$loc} = $heat;
-            $sum += $heat +1;
+for (my $r=0;$r<@map;$r++) {
+    for (my $c=0;$c<$map[$r]->@*;$c++) {
+        my $heat = $map[$r]->[$c];
+        my $lower=0;
+        for my $move ([-1,0],[1,0],[0,-1],[0,1]) {
+            my $val = $map[$r + $move->[0]]->[$c + $move->[1]];
+            if (not defined $val) {
+                $lower++;
+            }
+            elsif ($heat < $val) {
+                $lower++;
+            }
+            if ($lower == 4) {
+                $hits{$r.':'.$c} = $heat;
+                $sum += $heat +1;
+            }
         }
     }
 }
