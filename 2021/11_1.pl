205600ee083c168b49aca002004a1d2e7e573a47
Thomas Klausner <domm@plix.at>
2021-12-11 11:59:55 +0100

    day 11 part 1, straight forward, but cannot inc and flash in one loop

---
+++ 2021/11_1.pl
@@ -0,0 +1,57 @@
+use 5.030;
+use strict;
+use warnings;
+
+my @map;
+my ($r) = 0;
+for (<>) {
+    chomp;
+    my $c = 0;
+    for ( split(//) ) {
+        $map[$r]->[ $c++ ] = $_;
+    }
+    $r++;
+}
+my $flash=0;
+for my $step (1 .. 100) {
+    my @next;
+    for my $r (0..9) {
+        for my $c (0..9) {
+            $map[$r][$c]++;
+        }
+    }
+    for my $r (0..9) {
+        for my $c (0..9) {
+            flash($r, $c) if $map[$r][$c] > 9;
+        }
+    }
+    #show($step);
+}
+
+say $flash;
+
+sub flash {
+    my ($r, $c) = @_;
+    $map[$r][$c]=0;
+    $flash++;
+    for my $m ([-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]) {
+        my $rm = $r+$m->[0];
+        my $cm = $c+$m->[1];
+        next if $rm < 0 || $cm < 0 || $rm > 9 || $cm > 9;
+        next if $map[$rm][$cm] == 0;
+        $map[$rm][$cm]++;
+        flash($rm,$cm) if $map[$rm][$cm] > 9;
+    }
+}
+
+sub show {
+    my ($step) = @_;
+    say "After step $step";
+    for my $r (0..9) {
+        for my $c (0..9) {
+            print $map[$r][$c];
+        }
+        print "\n";
+    }
+}
+
