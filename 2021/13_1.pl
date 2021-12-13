95ae9104e9dd6549af3994af7a96ab106bbdacda
Thomas Klausner <domm@plix.at>
2021-12-13 07:40:16 +0100

    part one, uff

--- 2021/13_1.pl
+++ 2021/13_1.pl
@@ -5,9 +5,9 @@ use feature qw(signatures);
 no warnings qw(experimental::signatures);
 
 my ($map, $fold) = split(/\n\n/,join('',<>));
-
+say $fold;
 my @map;
-my ($max_c,$max_r)=(0,0);
+my ($max_r,$max_c)=(0,0);
 for (split(/\n/,$map)) {
     chomp;
     last unless /\d/;
@@ -23,32 +23,77 @@ for my $r (0..$max_r) {
     }
 }
 
-#draw(\@map);
-
-for (split(/\n/,$fold)) {
+draw(\@map);
+for my $line (split(/\n/,$fold)) {
+    say "LINE $line";
     my @new;
-    if (/y=(\d+)/) {
-        my $fold = $1;
-        say $fold;
-        splice(@map,$fold,1);
-        my @low = splice(@map,$fold,@map-$fold);
-        draw(\@map);
-        say 'xxxxx';
-        draw(\@low);
+    if ($line =~ /y=(\d+)/) {
+        my @new = fold($1,@map);
+        #   my $fold = $1;
+        #   splice(@map,$fold,1);
+        #   my @low = splice(@map,$fold,@map-$fold);
+        #   draw(\@map);
+        #   say 'xxxxx';
+        #   draw(\@low);
 
-        say 'fold';
+        #   say 'fold';
+        #   my $r=0;
+        #   for my $folded (reverse @low) {
+        #       for (my $c=0;$c<@{$folded};$c++) {
+        #           my $spot = $folded->[$c];
+        #           $map[$r][$c] = $spot if $spot eq '#';
+        #       }
+        #       $r++;
+        #   }
+        #   draw(\@map);
+        # draw(\@new);
+    @map = @new;
+    }
+    elsif ($line =~ /x=(\d+)/) {
+        my $at = $1;
         my $r=0;
-        for my $folded (reverse @low) {
-            for (my $c=0;$c<@{$folded};$c++) {
-                my $spot = $folded->[$c];
-                $map[$r][$c] = $spot if $spot eq '#';
+        for my $row (@map) {
+            splice(@$row,$at,1);
+            my @right = splice(@$row,$at,@$row-$at);
+            my $c=0;
+            for my $mark (reverse @right) {
+                $map[$r][$c] = $mark if $mark eq '#';
+                $c++;
             }
             $r++;
         }
-        draw(\@map);
 
     }
+    draw(\@map);
+    count(\@map);
     exit;
+}
+
+sub count {
+    my $map = shift;
+    my $cnt=0;
+    for my $r (@$map) {
+        for my $c (@$r) {
+            $cnt++ if $c eq '#';
+        }
+    }
+    say $cnt;
+}
+
+
+sub fold {
+    my ($fold, @map) = @_;
+    splice(@map,$fold,1);
+    my @low = splice(@map,$fold,@map-$fold);
+    my $r=0;
+    for my $folded (reverse @low) {
+        for (my $c=0;$c<@{$folded};$c++) {
+            my $spot = $folded->[$c];
+            $map[$r][$c] = $spot if $spot eq '#';
+        }
+        $r++;
+    }
+    return @map;
 }
 
 
