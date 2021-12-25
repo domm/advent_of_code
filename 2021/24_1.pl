8b4a1b148ebaf7e50ea3d43c851523a0b3feabcc
Thomas Klausner <domm@plix.at>
2021-12-25 19:50:34 +0100

    melting my cores again

--- 2021/24_1.pl
+++ 2021/24_1.pl
@@ -3,16 +3,29 @@ use strict;
 use warnings;
 
 my @prog = map { chomp; [ split/ / ] } <>;
-my %var;
-for (1..8) {
-    say "$_ ". run($_);
-    say $var{w}.$var{x},$var{y}.$var{z};
+
+#my $mnr = 99999999999999;
+my $mnr = 99999996883287;
+
+my $cnt=0;
+while (1) {
+    $mnr--;
+    next if $mnr =~ /0/;
+    if ($cnt++ > 100_000) {
+        say $mnr;
+        $cnt=0;
+    }
+    my @data=split(//,$mnr);
+    my $result = run(@data);
+    if ($result->{z} == 0) {
+        say "found $mnr";
+    }
 }
 
 sub run {
     my ( @input ) =@_;
 
-    %var = (w=>0,x=>0,y=>0,z=>0);
+    my %var = (w=>0,x=>0,y=>0,z=>0);
     for my $statement (@prog) {
         my ($op, $a, $b ) = $statement->@*;
 
@@ -20,8 +33,9 @@ sub run {
             $var{$a} = shift(@input);
             next;
         }
+        $var{a} //= 0;
         if ($b =~/[wxyz]/) {
-            $b = $var{$b};
+            $b = $var{$b} // 0;
         }
         #        say "call $op with $a, $b";
         if ($op eq 'add') {
@@ -36,12 +50,13 @@ sub run {
         elsif ($op eq 'mod') {
             $var{$a} = $var{$a} % $b;
         }
-        elsif ($op eq 'eq') {
+        elsif ($op eq 'eql') {
             $var{$a} = 1 if $var{$a} == $b;
         }
         else {
             die "invalid op $op";
         }
     }
+    return \%var;
 }
 
