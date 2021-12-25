534eede0a8aeab5057b9f1b5a66a111f02a275fd
Thomas Klausner <domm@plix.at>
2021-12-25 21:28:36 +0100

    but I can at least parse the rules and let the computer figure out the numbers (writing this took way longer than solving by hand..). Still fun!

--- 2021/24_1.pl
+++ 2021/24_1.pl
@@ -4,6 +4,9 @@ use warnings;
 
 # https://github.com/dphilipson/advent-of-code-2021/blob/master/src/days/day24.rs
 
+# read the "code" and store it in 14 chunks (each starting with
+# getting one input)
+
 my @progs;
 my $i = 0;
 for (<>) {
@@ -13,8 +16,13 @@ for (<>) {
     push($progs[$i]->@*,\@line);
 }
 
+# go through each code-chunk, and look at line 6 and 16, which contain
+# the "check" and the "offset" value, which we can use to build the
+# rules for weach position of the code
+# see here: https://github.com/dphilipson/advent-of-code-2021/blob/master/src/days/day24.rs
+
 my @stack;
-my @rules;
+my %rules;
 for my $i (1..14) {
     my $prog = $progs[$i];
     my $check = $prog->[5]->[2];
@@ -25,11 +33,33 @@ for my $i (1..14) {
     else {
         my $old = pop(@stack);
         my $calc = $old->[1] + $check;
-        push(@rules, ("[$i] = [".$old->[0]. "] ".($calc>0?'+':'-')." ".abs($calc)));
+        $rules{$i} = [$old->[0],$calc];
     }
 }
 
-say "and now manually solve the val of each pos:";
+# now we have rules like
+#  '13' => [ 0, -2 ],
+# which means that the val at pos 13 has to be the value from pos 0 minus 2
+# if we're looking for the highest values, this means that pos 0 has to be 9, and pos 13 has to be 7
 
-use Data::Dumper; $Data::Dumper::Maxdepth=3;$Data::Dumper::Sortkeys=1;warn Data::Dumper::Dumper \@rules;
+# so we can now autosolve this:
+my @high=' ';
+my @low= ' ';
+while (my ($pos, $rule) = each %rules) {
+    if ($rule->[1] <= 0) {
+        $high[$rule->[0]] = 9;
+        $high[$pos] = 9 + $rule->[1];
+        $low[$pos]=1;
+        $low[$rule->[0]]= 1 - $rule->[1] ;
+    }
+    else {
+        $high[$pos] = 9;
+        $high[$rule->[0]] = 9 - $rule->[1];
+        $low[$rule->[0]] = 1;
+        $low[$pos] = 1 + $rule->[1];
+    }
+}
+
+say "high: ".join('',@high);
+say "low:  ".join('',@low);
 
