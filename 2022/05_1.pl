ff8516f6569b89d584df822573c246f90a20d554
Thomas Klausner <domm@plix.at>
2022-12-05 11:06:20 +0100

    day 5 1

---
+++ 2022/05_1.pl
@@ -0,0 +1,28 @@
+use 5.030;
+use strict;
+use warnings;
+
+
+my ($in_stacks, $moves) = split(/\n\n/,join('',<>));
+
+my @stacks;
+for my $line (split(/\n/,$in_stacks)) {
+    next if $line =~/1/;
+    my @line = split(//,$line);
+    my @vals = @line[1,5,9,13,17,21,25,29,33];
+    my $stack = 1;
+    for my $crate (@vals) {
+        push($stacks[$stack]->@*, $crate) if $crate =~/\w/;
+        $stack++;
+    }
+}
+
+for my $move (split(/\n/,$moves)) {
+    my ($cnt, $from, $to) = $move =~/move (\d+) from (\d+) to (\d+)/;
+    for (1..$cnt) {
+        unshift($stacks[$to]->@*,shift($stacks[$from]->@*));
+    }
+}
+
+say join('',map { $_->[0]  } @stacks[1 .. @stacks])
+
