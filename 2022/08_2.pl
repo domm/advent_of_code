7ea52d0074e108485fe687813b6c91e98c51308a
Thomas Klausner <domm@plix.at>
2022-12-08 19:10:27 +0100

    day 8 part 2 much nicer!

---
+++ 2022/08_2.pl
@@ -0,0 +1,28 @@
+use v5.36;
+use strict;
+use warnings;
+
+my @grid = map {chomp; [ split//  ]} <>;
+my $max=0;
+for my $r ( 0 .. $#grid) {
+    for my $c ( 0 .. $#grid) {
+        my $tree = $grid[$r][$c];
+        my $scenic = 1;
+        for my $dir ([-1,0],[0,1],[1,0],[0,-1]) {
+            my $dist = 0;
+            my $lr = $r;
+            my $lc = $c;
+            while (1) {
+                $lr += $dir->[0];
+                $lc += $dir->[1];
+                last if ($lr < 0 || $lc < 0 || $lr > $#grid || $lc > $#grid);
+                my $look = $grid[$lr][$lc];
+                $dist++;
+                last if $look >= $tree;
+            }
+            $scenic *= $dist;
+        }
+        $max = $scenic if $scenic > $max;
+    }
+}
+say $max;
