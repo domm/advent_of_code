5f4030f35b69552c2f6fa4a5045e36c6c8d58151
Thomas Klausner <domm@plix.at>
2022-12-09 10:25:29 +0100

    day 9 1 works, but could be done smarter for sure...

--- 2022/09_1.pl
+++ 2022/09_1.pl
@@ -8,20 +8,41 @@ my %seen;
 my %move = ( R=>[1,1],L=>[1,-1],U=>[0,-1],D=>[0,1]);
 for my $in (<>) {
     my ($dir,$steps) = $in=~/(\w) (\d+)/;
-    say "Go $steps in $dir";
     my ($dim,$factor) = $move{$dir}->@*;
-
-    $head[$dim] += $factor * $steps;
-
-    draw();
-
+    for (1 .. $steps) {
+        $head[$dim] += $factor;
+        my $dr = abs($head[0] - $tail[0]);
+        my $dc = abs($head[1] - $tail[1]);
+        if ($dr == 2 && $dc == 1) {
+            $tail[0] = $head[0] - $factor;
+            $tail[1] = $head[1];
+        }
+        elsif ($dr == 1 && $dc == 2) {
+            $tail[1] = $head[1] - $factor;
+            $tail[0] = $head[0];
+        }
+        elsif ($dr > 1) {
+            $tail[0] += $factor;
+        }
+        elsif ($dc > 1) {
+            $tail[1] += $factor;
+        }
+        $seen{join(':',@tail)}++;
+    }
 }
 
+say scalar keys %seen;
+
+#draw();
+
 sub draw{
-    for my $r (-10 .. 10) {
-        for my $c (-10 .. 10) {
+    for my $r (-5 .. 5) {
+        for my $c (-5 .. 5) {
             if ($head[0] == $r && $head[1] eq $c) {
                 print "H";
+            }
+            elsif ($tail[0] == $r && $tail[1] eq $c) {
+                print "T";
             }
             elsif ($r ==0 && $c ==0) {
                 print 's';
