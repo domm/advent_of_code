7a3b7329d934f744b35cbec146e4a2ffb68639c5
Thomas Klausner <domm@plix.at>
2023-12-11 20:49:06 +0100

    day 11 part 1: after getting the expension right (and using an array of galaxies and not using a 2d map) getting the distances is easy

--- 2023/11_1.pl
+++ 2023/11_1.pl
@@ -33,7 +33,21 @@ my $expand = 1;
 expand($expand, 'r', \@empty_rows, \%rows, \@map);
 expand($expand, 'c', \@empty_cols, \%cols, \@map);
 
-draw();
+my %dist;
+my $sum;
+for my $g (@map) {
+    my $from = $g->{g};
+    for my $t (@map) {
+        my $to = $t->{g};
+        my $id = join('-',sort($from, $to));
+        next if $to eq $from || $dist{$id};
+        my $dist = $dist{$id} = abs($g->{r} - $t->{r}) + abs($g->{c} - $t->{c});
+        $sum+=$dist;
+    }
+}
+
+say $sum;
+#draw();
 
 sub expand {
     my ($expand, $dim, $empty, $onedim, $map) = @_;
@@ -41,9 +55,9 @@ sub expand {
 
         for my $g (sort { $b->{$dim} <=> $a->{$dim}} @$map ) {
             next unless $g->{$dim} > $v;
-            say $g->{g}. " $v -> ".$g->{$dim};
+            #say $g->{g}. " $v -> ".$g->{$dim};
             my $new = $g->{$dim} + ($expand );
-            say "  move to ".$new;
+            #say "  move to ".$new;
             $g->{$dim} = $new;
         }
     }
