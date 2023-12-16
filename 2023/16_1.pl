80a972843626e1828464735a874f8d3d9a1af890
Thomas Klausner <domm@plix.at>
2023-12-16 13:04:14 +0100

    day 16 part 1: after more thinking I figured out that we only need to handle each beam passing an location in a given direction once

--- 2023/16_1.pl
+++ 2023/16_1.pl
@@ -15,10 +15,8 @@ my %move = (
     S=>[1,0],
     W=>[0,-1],
 );
-my $b=1;
-my @beams = ( { pos=>[0,0],dir=>'E', id=>$b } );
+my @beams = ( { pos=>[0,0],dir=>'E' } );
 my %powered;
-my %seen;
 
 my @map = map { chomp; [split//] } <>;
 my $max_r = scalar @map;
@@ -27,18 +25,18 @@ my $max_c = scalar $map[0]->@*;
 my $s=0;
 while (@beams) {
     my $beam = shift(@beams);
-    $powered{ join(':',$beam->{pos}->@*) }++;
+    my $pos = join(':',$beam->{pos}->@*);
+    next if $powered{ $pos }{$beam->{dir}}++;
+
     my $action = $rules{$map[$beam->{pos}[0]][$beam->{pos}[1]]};
-    my $nb=0;
     for my $dir (split(//,$action->{$beam->{dir}})) {
         my $new_r = $beam->{pos}[0] + $move{$dir}[0];
         my $new_c = $beam->{pos}[1] + $move{$dir}[1];
         if ($new_r >= 0 && $new_r < $max_r && $new_c >=0 && $new_c < $max_c) {
-            my $new = { pos=>[ $new_r, $new_c ], dir=>$dir, id => $beam->{id} + $nb++};
+            my $new = { pos=>[ $new_r, $new_c ], dir=>$dir };
             push(@beams, $new);
         }
     }
-    my $powered_cnt = scalar keys %powered;
-    last if $seen{$powered_cnt}++ > 1000000;
 }
 say scalar keys %powered;
+
