d127aad148248bce03390042ddf218888e508ea8
Thomas Klausner <domm@plix.at>
2025-12-11 22:08:10 +0100

    day 10_1: convert to ints (so we can do XOR); tried BFS but got lost in my ignorance, so just again use Algorithm::Combinatorics to iterate to ever increasing combinations of button presses

--- 2025/10_1.pl
+++ 2025/10_1.pl
@@ -2,22 +2,44 @@
 
 use v5.40;
 
+use Algorithm::Combinatorics qw(combinations);
+
 my $total;
 for (<>) {
+
+    # very convoluted way to convert the input into ints
     my ($target, $buttons, $joltage) = $_ =~ /^\[(.*?)\] (.*) \{(.*)\}/;
     $target=~tr/.#/01/;
     my $tb = oct("0b".$target);
     my $max = oct("0b". ('1' x length($target)));
+    my $start=('0' x length($target));
 
     my @bb;
     for my $bdef (split(/ /, $buttons)) {
         $bdef=~s/[()]//g;
-        my $b=('0' x length($target));
+        my $b=$start;
         for my $pos ( split(/,/, $bdef)) {
             substr($b, $pos, 1, 1);
         }
         push(@bb, oct("0b".$b));
     }
 
-    say "try to get from 0 to $tb via a bitcombination of ".join(',',@bb);
+    # try larger combinations until we find one that matches
+    my $tries;
+    TRY: for my $k (1 .. @bb) {
+        my $iter = combinations(\@bb, $k);
+        while (my $btns = $iter->next) {
+            my $l = $start;
+            for my $btn (@$btns) {
+                $l = $l ^ $btn; # xor the ints = add the bitmasks
+                if ($l == $tb) {
+                    $tries = $k;
+                    last TRY;
+                }
+            }
+        }
+    }
+    $total+=$tries;
 }
+say "total $total";
+
